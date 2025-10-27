35613dc2e8be88e8ec35a8223b5578a46747eaaf
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:11:14 +0800

    Square Function

--- Calculator4.js
+++ Calculator4.js
@@ -14,9 +14,14 @@ function division(a,b){
     return a/b
 }
 
+function square(a){
+    return a*a
+}
+
 let a = 5
 let b = 3
 console.log("ADD:", add(a, b))
 console.log("SUBTRACTION:", subtraction(a, b))
 console.log("MULTIPLICATION:", multiplication(a, b))
 console.log("DIVISION:", division(a, b))
+console.log("DIVISION:", square(a))
