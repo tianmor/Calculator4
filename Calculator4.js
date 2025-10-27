02eb82385f2ceef9e400f5b246723a34e6cc0383
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:09:05 +0800

    Multiplication Function

--- Calculator4.js
+++ Calculator4.js
@@ -6,7 +6,12 @@ function subtraction(a,b){
     return a-b
 }
 
+function multiplication(a,b){
+    return a*b
+}
+
 let a = 5
 let b = 3
 console.log("ADD:", add(a, b))
 console.log("SUBTRACTION:", subtraction(a, b))
+console.log("MULTIPLICATION:", multiplication(a, b))
