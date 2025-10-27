ebfbb47cc9609032334b1a9e8ea71944ba6d2161
tian403 <morando.markchristian@gmail.com>
2025-10-27 22:09:52 +0800

    Division Function

--- Calculator4.js
+++ Calculator4.js
@@ -10,8 +10,13 @@ function multiplication(a,b){
     return a*b
 }
 
+function division(a,b){
+    return a/b
+}
+
 let a = 5
 let b = 3
 console.log("ADD:", add(a, b))
 console.log("SUBTRACTION:", subtraction(a, b))
 console.log("MULTIPLICATION:", multiplication(a, b))
+console.log("DIVISION:", division(a, b))
